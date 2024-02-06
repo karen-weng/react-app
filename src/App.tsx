36fbac2e31d6f7b40a236b81b2732edb82afae65
Karen Weng <karen.wengxt@gmail.com>
2024-02-05 19:21:41 -0500

    made a list

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import ListGroup from "./components/ListGroup";
+import React from 'react'
+
+function App() {
+  let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
+
+  return (
+    <div>
+      <ListGroup items={items} heading="Cities" />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Message.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+function Message() {
+    
+    const name = "Angela";
+    return <h1>Hello {name}</h1>;
+}
+export default Message;
