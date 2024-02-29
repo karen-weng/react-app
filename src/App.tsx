04c8abd94d5a5cfff6c186f1efcded9caad5a727
Karen Weng <karen.wengxt@gmail.com>
2024-02-29 16:59:40 -0500

    More names

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,25 @@ import ListGroup from "./components/ListGroup";
 import React from "react";
 
 function App() {
-  let items = ["DunnyTM", "Karen", "Panos", "Hannalee", "Angela", "Joanne"];
+  let items = [
+    "DunnyTM",
+    "Hannalee",
+    "Jason",
+    "Angela",
+    "Anne",
+    "Angeli",
+    "Ryu",
+    "Katie",
+    "Margaret",
+    "Joanne",
+    "Sunny",
+    "Eric",
+    "Panos",
+    "Nathaniel",
+    "Evan",
+    "Leo",
+    "Karen",
+  ];
 
   const handleSelectItem = (item: string) => {
     console.log(item);
